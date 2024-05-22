fe75ec1faa4704da5c8a560715c3400c943f0ee3
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-22 22:52:02 +0300

    Add IDriveable interface and dependacy with CarMechanic class.

--- CarRental/Models/Car/IDriveable.cs
+++ CarRental/Models/Car/IDriveable.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CarRental.Models.Car;
 
-internal class CarMechanic : ICarMechanics, IDriveable
+internal interface IDriveable
 {
-
+    public void Drive();
 }
