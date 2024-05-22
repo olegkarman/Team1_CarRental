e155df9c98d87dc0bf2d965d19c2737fef47b254
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-22 17:19:18 +0300

    Add enum MaterialInterior, add property KnownColor Color to Interior.cs file.

--- CarRental/Models/Car/MaterialInterior.cs
+++ CarRental/Models/Car/MaterialInterior.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
 
 namespace CarRental.Models.Car;
 
-internal class CarInterior
+internal enum MaterialInterior
 {
-    // FROM System.Drawing, BASE COLORS ENUM.
-    public KnownColor Color { get; set; }
-
-
+    unknown = 0,
+    metal = 10,
+    plastic = 11,
+    leather = 12,
+    carbone = 13,
+    wood = 14
 }
