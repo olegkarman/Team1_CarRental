21b41b494ed2a26c660497ced91b167f09c65833
DMIbragimov <daniilibragimov466@gmail.com>
2024-05-22 18:55:26 +0300

    added some properties

--- CarRental/Models/Customer.cs
+++ CarRental/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,24 @@ using System.Threading.Tasks;
 namespace CarRental.Models;
 internal class Customer
 {
+    public const float BasicDiscount = 0.5f;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public readonly string IdNumber;
+    public required string DrivingLicenseNumber { get; set; }
+
+    [SetsRequiredMembers]
+    public Customer(string? firstName, string? lastName, string idNumber, string drivingLicenseNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        IdNumber = idNumber;
+        DrivingLicenseNumber = drivingLicenseNumber;
+    }
+
+
+
+
     /*FirstName: string
     LastName: string
     IdNumber: string
