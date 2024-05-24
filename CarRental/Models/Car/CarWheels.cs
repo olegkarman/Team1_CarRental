8a59673c814dca38ed0367075e81f5103d637b2d
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-24 05:42:46 +0300

    Add constructors to CarWheel class.

--- CarRental/Models/Car/CarWheels.cs
+++ CarRental/Models/Car/CarWheels.cs
@@ -14,4 +14,22 @@ internal class CarWheels : AbstractWheels
     public override required int Size { get; init; }
     public override required TypeTire Tire { get; set; }
     public override required ComponentStatus Status { get; set; }
+
+    // CONSTRUCTORS
+
+    public CarWheels()
+    {
+        this.Material = 0;
+        this.Size = 0;
+        this.Tire = 0;
+        this.Status = 0;
+    }
+
+    public CarWheels(MaterialWheel material, int size, TypeTire tire, ComponentStatus status)
+    {
+        this.Material = material;
+        this.Size = size;
+        this.Tire = tire;
+        this.Status = status;
+    }
 }
