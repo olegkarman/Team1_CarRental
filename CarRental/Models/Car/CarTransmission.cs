363ca0ce87743bffa4d10afcaec647d1b8eca5b9
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-24 05:37:05 +0300

    Add contructors to CarTransmission class.

--- CarRental/Models/Car/CarTransmission.cs
+++ CarRental/Models/Car/CarTransmission.cs
@@ -8,10 +8,32 @@ namespace CarRental.Models.Car;
 
 internal class CarTransmission : AbstractTransmission
 {
+    // FIELDS
+
+    private const string _noInfo = "NO INFO";
+
     // PROPERTIES
 
     internal override required string SerialNumber { get; init; }
     public override required TypeTransmission Type { get; init; }
     public int SpeedCount { get; init; }
     public override required ComponentStatus Status { get; set; }
+
+    // CONSTRUCTORS
+
+    public CarTransmission()
+    {
+        this.SerialNumber = _noInfo;
+        this.Type = 0;
+        this.SpeedCount = 0;
+        this.Status = 0;
+    }
+
+    public CarTransmission(string serialNumber, TypeTransmission type, int speedCount, ComponentStatus status)
+    {
+        this.SerialNumber = serialNumber;
+        this.Type = type;
+        this.SpeedCount = speedCount;
+        this.Status = status;
+    }
 }
