9a8af575062360289c94eaaca8e05f5efea999a0
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-25 21:39:15 +0300

    Add CarRecord basic class-carcass.

---
+++ CarRental/Models/Car/CarRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Models.Car;
+
+internal class CarRecord
+{
+    // ID OF RECORD.
+    // CAR VIN CODE.
+    // CURRENT CAR NUMBER.
+    // CREATION DATE.
+    // CURRENT OWNER.
+    // INSURANCE.
+    // PRICE.
+    // IS FIT FOR USE?
+    // INSPECTION[S].
+    // REPAIR[S].
+}
