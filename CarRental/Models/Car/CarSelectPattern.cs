a547695196bb7404b992fed03658168c4d0ac6d9
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-26 13:44:06 +0300

    Add default constructor for CarSelectPattern class.

--- CarRental/Models/Car/CarSelectPattern.cs
+++ CarRental/Models/Car/CarSelectPattern.cs
@@ -15,4 +15,12 @@ internal class CarSelectPattern : ICarSelectivePattern
     // PROPERTIES
 
     public string PatternName { get; init; }
+
+    // CONSTRUCTORS
+
+    public CarSelectPattern()
+    {
+        this._charMap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        this.PatternName = "DefaultPattern";
+    }
 }
