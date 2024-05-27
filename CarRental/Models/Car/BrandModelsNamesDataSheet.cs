b042a3e48765c8d0d19c3418030a97a99b1a32ca
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-27 19:32:49 +0300

    Add some more orginized data collection to the corresponding class.

--- CarRental/Models/Car/BrandModelsNamesDataSheet.cs
+++ CarRental/Models/Car/BrandModelsNamesDataSheet.cs
@@ -12,11 +12,12 @@ internal record class BrandModelsNamesDataSheet
 
     internal string[] BrandNamesData { get; init; }
     internal string[] ModelNamesData { get; init; }
-    internal Dictionary<string, string> ModelsZaporozhets { get; init }
-    internal Dictionary<string, string> ModelsVolkswagen { get; init }
-    internal Dictionary<string, string> ModelsNissan { get; init }
-    internal Dictionary<string, string> ModelsGyguli { get; init }
-    internal Dictionary<string, string> ModelsJeep { get; init }
+    internal Dictionary<string, string> ModelsZaporozhets { get; init; }
+    internal Dictionary<string, string> ModelsPeugeot { get; init; }
+    internal Dictionary<string, string> ModelsVolkswagen { get; init; }
+    internal Dictionary<string, string> ModelsNissan { get; init; }
+    internal Dictionary<string, string> ModelsGyguli { get; init; }
+    internal Dictionary<string, string> ModelsJeep { get; init; }
     internal Dictionary<string, int[]> ModelPatternDataDictionary { get; init; }
 
     // CONSTRUCTORS
@@ -44,6 +45,48 @@ internal record class BrandModelsNamesDataSheet
             // 5 — JEP [43, 52]
             "Dakar", "Rubicon", "Malibu", "Wide-Trac", "Cherokee", "Creep", "Cowboy", "Freedom", "Wrangler", "Ecco"
         ];
+
+        this.ModelsZaporozhets = new Dictionary<string, string>();
+
+        for (int index = 0; index < 3; index = index + index)
+        {
+            this.ModelsZaporozhets.Add(this.ModelNamesData[index], this.BrandNamesData[0]);
+        }
+
+        this.ModelsPeugeot = new Dictionary<string, string>();
+
+        for (int index = 3; index < 14; index = index + index)
+        {
+            this.ModelsPeugeot.Add(this.ModelNamesData[index], this.BrandNamesData[1]);
+        }
+
+        this.ModelsVolkswagen = new Dictionary<string, string>();
+
+        for (int index = 14; index < 24; index = index + index)
+        {
+            this.ModelsVolkswagen.Add(this.ModelNamesData[index], this.BrandNamesData[2]);
+        }
+
+        this.ModelsNissan = new Dictionary<string, string>();
+
+        for (int index = 25; index < 35; index = index + index)
+        {
+            this.ModelsNissan.Add(this.ModelNamesData[index], this.BrandNamesData[3]);
+        }
+
+        this.ModelsGyguli = new Dictionary<string, string>();
+
+        for (int index = 36; index < 42; index = index + index)
+        {
+            this.ModelsGyguli.Add(this.ModelNamesData[index], this.BrandNamesData[4]);
+        }
+
+        this.ModelsJeep = new Dictionary<string, string>();
+
+        for (int index = 43; index < 52; index = index + index)
+        {
+            this.ModelsJeep.Add(this.ModelNamesData[index], this.BrandNamesData[5]);
+        }
 
         this.ModelPatternDataDictionary = new Dictionary<string, int[]>
         {
