cf94caaabbf9dbfceaf3819a85aa7b759c43f4a6
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-28 15:23:27 +0300

    Add CarServiceManage class-carcass.

---
+++ CarRental/Models/Car/CarServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Models.Car;
+
+public class CarServiceManager
+{
+    // GET NEW CAR
+    // GENERATE CAR LIST
+    // DELETE CAR FROM LIST
+    // GET CAR INFO
+}
