d4b5fa9c5db1e052fa04504f20a8bca57c61bf1c
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-05-28 15:36:39 +0300

    Replace logic of BrandRecord instances creation from WYCDepot to portal-class. It commented.

--- CarRental/Models/Car/WeylandYutaniCarRepairShopPortal.cs
+++ CarRental/Models/Car/WeylandYutaniCarRepairShopPortal.cs
@@ -13,6 +13,85 @@ public class WeylandYutaniCarRepairShopPortal
         Console.WriteLine("        .\r\n       ,O,\r\n      ,OOO,\r\n'oooooOOOOOooooo'\r\n  `OOOOOOOOOOO`\r\n    `OOOOOOO`\r\n    OOOO'OOOO\r\n   OOO'   'OOO\r\n  O'         'O\n\u2606☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆\n☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆\n☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆\nWE PROUD TO WELCOME YOU IN WEYLAND-YUTANI CAR REPAIR SHOP!!!\n☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆\n☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆\n☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆");
 
         WYCDepot depot = new CarRental.Models.Car.WYCDepot();
+        // /**/
+        // BrandModelsNamesDataSheet dataSheet = new BrandModelsNamesDataSheet();
+        // CarServiceManager serviceManager = new CarServiceManager();
+
+        /*internal void SetBrandRecords(BrandModelsNamesDataSheet brandModelsNamesDataSheet)
+    {
+        // DATA INITIALIZATION
+        
+        //this._brandModelData = new BrandModelsNamesDataSheet();
+        //this._patternInit = new PatternInitializator(_brandModelData);
+
+        this._records =
+        [
+            // ZAPOROZHETS
+            new BrandRecord
+            (
+                // ID
+                (DateTime.Now.ToString() + _brandModelData.BrandNamesData[0].ToUpper()),
+
+                // ZAPOROZHETS
+                _brandModelData.BrandNamesData[0],
+
+                // SELECTS PROPER NAMES FOR THE ARRAY AND COPY IT INTO AN ARRAY AND THEN INTO RECORD-CLASS.
+
+                _brandModelData.ModelNamesData[0..2]
+            ),
+
+            // PEUGEOT
+            new BrandRecord
+            (
+                (DateTime.Now.ToString() + _brandModelData.BrandNamesData[1].ToUpper()),
+
+                _brandModelData.BrandNamesData[1],
+
+                _brandModelData.ModelNamesData[3..13]
+            ),
+
+            // VOLKSWAGEN
+            new BrandRecord
+            (
+                (DateTime.Now.ToString() + _brandModelData.BrandNamesData[2].ToUpper()),
+
+                _brandModelData.BrandNamesData[2],
+
+                _brandModelData.ModelNamesData[14..24]
+            ),
+
+            // NISSAN
+            new BrandRecord
+            (
+                (DateTime.Now.ToString() + _brandModelData.BrandNamesData[3].ToUpper()),
+
+                _brandModelData.BrandNamesData[3],
+
+                _brandModelData.ModelNamesData[25..35]
+            ),
+
+            // GYGULI
+            new BrandRecord
+            (
+                (DateTime.Now.ToString() + _brandModelData.BrandNamesData[4].ToUpper()),
+
+                _brandModelData.BrandNamesData[4],
+
+                _brandModelData.ModelNamesData[36..42]
+            ),
+
+            // JEEP
+            new BrandRecord
+            (
+                (DateTime.Now.ToString() + _brandModelData.BrandNamesData[5].ToUpper()),
+
+                _brandModelData.BrandNamesData[5],
+
+                _brandModelData.ModelNamesData[43..52]
+            )
+        ];*/
+
+
         Console.WriteLine(depot.GetNewCar());
 
         // VIEW CAR TECHNICAL INFO.
