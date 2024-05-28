b8da58544546bc258e77c415117086c974c24704
Nina Babinets <ninadanulo@gmail.com>
2024-05-28 20:41:41 +0200

    check it

--- CarRental/Models/Car.cs
+++ CarRental/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,9 @@ internal class Car
     SendToRepair()
     Refuel()
     RemoveCar()*/
+
 }
+
 
 
 
--- CarRental/Models/Inspector.cs
+++ CarRental/Models/Inspector.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace CarRental.Models;
 internal class Inspector
 {
-    public readonly DateTime EmploymentDate; //Додати readonly поле (string Year)
+    private readonly DateTime EmploymentDate; //Додати readonly поле (string Year)
     private string firstName;
-    public static readonly int MaxCarsAllowed = 10;//Додати константу (const string InvalidCar ="Car has no mark")
 
     public string FirstName //Додати required проперті (string Id)
     {
@@ -23,98 +23,138 @@ internal class Inspector
             firstName = value;
         }
     }
-
     public string LastName { get; set; } //Додати звичайне проперті (string Mark)
-    public string IdNumber { get; set; }
-    public List<Car> cars;
-    public Inspector(string firstName, string lastName, string idNumber, DateTime employmentDate)
+    public int IdNumber { get; set; }
+    public Inspector()
     {
+        EmploymentDate = DateTime.MinValue;
+        firstName = "";
+        LastName = "";
+        IdNumber = 0;
+    }
+    public Inspector(string firstName, string lastName, int idNumber, DateTime employmentDate)
+    {
+
         FirstName = firstName;
         LastName = lastName;
         IdNumber = idNumber;
         EmploymentDate = employmentDate;
-        cars = new List<Car>();
+    }
+    public Inspector(string firstName, string lastName) : this(firstName, lastName, 0, DateTime.Now)
+    {
+        Console.WriteLine("Inspector object created with default employment date."); // Додати на нього оверлоадінг overloading. 
     }
     public string GetInspectorInfo() //Додати метод (string GetCarInfo() який виводить Id, Year та Mark)
     {
-        return $"Name: {FirstName} {LastName}, ID: {IdNumber}, Employment Date: {EmploymentDate}";
+        return $"Inspector: {FirstName} {LastName}, ID: {IdNumber}, Employment Date: {EmploymentDate.ToShortDateString()}";
     }
-
-    public void SomeMethod()
+    public void ChangeToInspector()
     {
-        Inspector inspector = new Inspector("Adam", "Smith", "12345", DateTime.Now);
-        string inspectorInfo = inspector.GetInspectorInfo();
-        Console.WriteLine(inspectorInfo);
-    }
-    public Inspector(string firstName, string lastName, string idNumber)
-    : this(firstName, lastName, idNumber, DateTime.Now)
-    {
-        // Додати на нього оверлоадінг overloading. 
-        Console.WriteLine("Inspector object created with default employment date.");
-    }
-
-   
-    public void InspectCar(List<Car> cars, int carId, Inspector inspector)
-    {
-        if (inspector.cars.Count <= Inspector.MaxCarsAllowed)
+        DateTime employmentDate = new DateTime(2020, 1, 1);
+        Inspector[] inspectors = new Inspector[]      // Створення трьох інспекторів і додавання їх до масиву
         {
-            foreach (Car car in cars)
-            {
-                if (car.SerialNumber == carId)
-                {
-                    if (car.IsFitForUse)
-                    {
-                        Console.WriteLine($"Vehicle {carId} is fit for use.");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Vehicle {carId} is unfit for use.");
-                    }
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine($"Vehicle {carId} not found.");
-                }
-            }
+            new Inspector("Adam", "Smith", 1001, employmentDate),
+            new Inspector("Eva", "Smith", 1002, employmentDate.AddYears(1)), // Інша дата працевлаштування
+            new Inspector("Mike", "Smith", 1003, employmentDate.AddYears(2))
+        };
+
+        Random random = new Random();
+        int index = random.Next(0, inspectors.Length); // Випадковий вибір інспектора
+
+        Inspector selectedInspector = inspectors[index];
+
+        Console.WriteLine($"Selected Inspector: {selectedInspector.FirstName} {selectedInspector.LastName}, ID: {selectedInspector.IdNumber}");
+    }
+}
+
+
+
+
+
+internal class InspectCars
+{
+    public int Mileage { get; set; }
+    public int ReleaseDate { get; set; }
+    public int ExteriorCondition { get; set; }
+
+    public const int MaxCarsAllowed = 10; //Додати константу (const string InvalidCar ="Car has no mark")
+    private int CurrentCarsInspected { get; set; }
+    public InspectCars Status { get; set; }
+    internal InspectCars()
+    {
+        Random rand = new Random();
+        Mileage = rand.Next(0, 300000);       // Пробіг від 0 до 300 000 км
+        ReleaseDate = rand.Next(2000, 2023);  // Рік випуску від 2000 до 2024
+        ExteriorCondition = rand.Next(1, 11);
+        CurrentCarsInspected = 0;// Стан кузова від 1 до 10
+    }
+    public void DisplayInfoInspectCar()
+    {
+        Console.WriteLine($"Mileage: {Mileage}, Release Date: {ReleaseDate}, Exterior Condition: {ExteriorCondition}");
+    }
+    internal void InspectedCars (Car car)
+    {
+
+        if (CurrentCarsInspected >= MaxCarsAllowed)
+        {
+            Console.WriteLine($"Inspector cannot check more than {MaxCarsAllowed} cars.");
+            return;
+        }
+        else
+        {
             Console.WriteLine("Inspector can check more cars.");
         }
+    }
+    internal void InspectCar (Car car) //Інспектувати по пробігу, даті,стану кузова
+    {
+
+        if (Mileage < 200000 && ReleaseDate >= 2015 && ExteriorCondition >= 1)
+        {
+            Console.WriteLine($"Car {car.Brand} {car.Model} is fit for use.");
+        }
         else
         {
-            Console.WriteLine($"Inspector cannot check more than {Inspector.MaxCarsAllowed} cars.");
+            Console.WriteLine($"Car {car.Brand} {car.Model} is unfit for use.");
         }
-
     }
-  
-    public void RecordInspectionResult(int carId, bool isFitForUse)
+
+    public void RecordInspectionResult(Car car, TransportStatus inspectionResult)
     {
-        var car = cars.Find(c => c.SerialNumber == carId);
         if (car != null)
         {
-            car.IsFitForUse = isFitForUse;
-            Console.WriteLine($"Inspection result for Vehicle {carId}: {(isFitForUse ? "Fit" : "Unfit")}");
-            if (!isFitForUse)
+            switch (inspectionResult)
             {
-                RemoveCarIfUnfit(cars, carId);
+                case TransportStatus.available:
+                    car.Status = "Ready";
+                    break;
+                case TransportStatus.inRepair:
+                    car.Status = "Repair";
+                    RemoveCarIfUnfit(car);
+                    break;
+                case TransportStatus.unavailable:
+                    car.Status = "Broken";
+                    RemoveCarIfUnfit(car);
+                    break;
+                default:
+                    break;
             }
+            Console.WriteLine($"Inspection result for Car {car.Brand} {car.Model}: {inspectionResult}. Car is {car.Status}.");
         }
         else
         {
-            Console.WriteLine($"Vehicle {carId} not found.");
+            Console.WriteLine("Car not found.");
         }
     }
-
-    public void RemoveCarIfUnfit(List<Car> cars, int carId)
+    public void RemoveCarIfUnfit(Car car) // Видалити машину
     {
-        var car = cars.Find(c => c.SerialNumber == carId);
-        if (car != null && !car.IsFitForUse)
+        if (car.Status == "Repair" || car.Status == "Broken")
         {
-            cars.Remove(car);
-            Console.WriteLine($"\nCar with Id {carId} removed because it is unfit for use.");
+            Console.WriteLine($"Car {car.Brand} {car.Model} with Serial Number {car.SerialNumber} removed because it is unfit for use.");
         }
         else
         {
-            Console.WriteLine($"\nCar with Id {carId} is either not found or is not unfit for use.");
+            Console.WriteLine($"Car {car.Brand} {car.Model} with Serial Number {car.SerialNumber} is fit for use. No action taken.");
         }
     }
 }
+
