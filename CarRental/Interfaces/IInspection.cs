df89791781206a1d48b914a3bf20a17c585b9787
Oleksandr Rohan <o.rohanov@gmail.com>
2024-05-29 15:08:37 +0300

    initial commit. Add interface IInspection

---
+++ CarRental/Interfaces/IInspection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarHubTest
+{
+    internal interface IInspection
+    {
+        // Properties
+        int InspectionId { get; }
+        DateTime InspectionDate { get; }
+        string? InspectorName { get; }
+        int CarId { get; }
+        InspectionStatusType Result { get; }
+
+        // Methods
+        bool IsInspectionSuccessfully(int inspectionId);
+        bool IsInspectionSuccessfully(string inspectorName);
+    }
+}
