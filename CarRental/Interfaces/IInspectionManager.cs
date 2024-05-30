5fd3d3101990e0de18c7c899cd376b1bfe4df520
Oleksandr Rohan <o.rohanov@gmail.com>
2024-05-30 11:00:23 +0300

    Add, IInspectionManager та Inspection

---
+++ CarRental/Interfaces/IInspectionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarHubTest
+{
+    internal interface IInspectionManager
+    {
+        void AddInspection(Inspection inspection);
+        void RemoveInspection(Guid inspectionId);
+        Inspection? GetInspection(Guid inspectionId);
+        IEnumerable<Inspection> GetAllInspections();
+        void PrintAllInspections();
+    }
+}
