5fdde6dcd6379ac2b3c1757c06cd4d232bbd8d6d
DMIbragimov <daniilibragimov466@gmail.com>
2024-06-03 12:29:12 +0300

    added extention  method for Customer

---
+++ CarRental/Extentions/ExtentionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Models;
+
+namespace CarRental.Extentions;
+public static class CustomerExtention
+{
+    public static string GetCustomerDocuments(this Customer customer)
+    {
+        return $"{customer.DrivingLicenseNumber} {customer.PassportNumber}";
+    }
+}
--- CarRental/Models/Customer.cs
+++ CarRental/Models/Customer.cs
@@ -6,11 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CarRental.Models;
-internal class Customer : User
+public class Customer : User
 {
     public const float BasicDiscount = 0.5f;
     public string PassportNumber { get; init; }
     public required string DrivingLicenseNumber { get; set; }
+
+   
 
     [SetsRequiredMembers]
     public Customer(string? firstName, string? lastName, string passportNumber, string drivingLicenseNumber, DateTime dateOfBirth, string password, string userName) : base(firstName, lastName, dateOfBirth, password , userName)
