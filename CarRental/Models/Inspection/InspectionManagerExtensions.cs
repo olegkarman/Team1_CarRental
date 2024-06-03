43eef28fe9a55faccbb9b417534ada4f44c0f556
Oleksandr Rohan <o.rohanov@gmail.com>
2024-06-03 23:21:29 +0300

    Add Extension and Record

---
+++ CarRental/Models/Inspection/InspectionManagerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Models;
+
+namespace CarRental.Models.Inspection
+{
+    internal static class InspectionManagerExtensions
+    {
+        public static List<Inspection> GetSuccessfulInspections(this InspectionManager manager)
+        {
+            return InspectionManager.GetAllInspection()
+                                    .Where(i => i.Result == InspectionStatusType.Successfully)
+                                    .ToList();
+        }
+    }
+}
