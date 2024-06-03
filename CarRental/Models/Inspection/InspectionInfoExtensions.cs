5f27b9cc60632cf1a329a43536b51747ce61d7a1
Oleksandr Rohan <o.rohanov@gmail.com>
2024-06-03 23:54:57 +0300

    added InspectionInfoExtensions

---
+++ CarRental/Models/Inspection/InspectionInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarHubTest
+{
+    internal static class InspectionInfoExtensions
+    {
+        public static string GetInspectionSummary(this Inspection inspection)
+        {
+            return $"Inspection Summary:\n" +
+                   $"Inspection ID: {inspection.InspectionId}\n" +
+                   $"Inspector Name: {inspection.InspectorName}\n" +
+                   $"Car ID: {inspection.CarId}\n" +
+                   $"Inspection Date: {inspection.InspectionDate.ToShortDateString()}\n" +
+                   $"Result: {inspection.Result}\n";
+        }
+    }
+}
