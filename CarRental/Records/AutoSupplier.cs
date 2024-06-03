a029a5b81056d63ff152c5add492a49a899fb04e
DMIbragimov <daniilibragimov466@gmail.com>
2024-06-03 12:57:20 +0300

    added record type for AutoSupplier

---
+++ CarRental/Records/AutoSupplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Models.Car;
+
+namespace CarRental.Records;
+public record AutoSupplier
+{
+    public string SupplierId { get; init; }
+    public string CompanyName { get; init; }
+    public string ContactName { get; init; }
+    public string ContactPhone { get; init; }
+    public string Address { get; init; }
+}
