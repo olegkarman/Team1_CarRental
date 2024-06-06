a3f1e1ad85a40cd6714472ac654bcde7fcd30e8f
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-06-06 18:06:01 +0300

    Create Interface folder on the bussiness layer. Move files correspondly.

---
+++ CarRental.BussinessLayer/Interfaces/ICarManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Enumerables;
+using CarRental.Models.Car;
+
+namespace CarRental.Data.Interfaces;
+
+public interface ICarManager
+{
+    // public Car GetNewCar();
+    // public void DeleteCar();
+    // public bool TryChangeCarStatus(TransportStatus status);
+
+    public void MakeNewListOf15Cars();      // CREATE
+    public string DisplayCurrentCar();      // RETRIVE
+    public void RefillSelectedCar();        // UPDATE
+    public void DeleteAllCarsFromList();    // DELETE
+    
+}
---
+++ CarRental.BussinessLayer/Interfaces/ICarMechanics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using CarRental.Enumerables;
+using CarRental.Models.Car;
+using CarRental.Interfaces;
+
+
+// HILLEL, C# PRO COURSE, TEACHER: MARIIA DZIVINSKA
+// HOMEWORK: "ДЗ 3. Methods, properties"
+// STUDENT: PARKHOMENKO YAROSLAV
+// DATE: 30-MAY-2024
+
+namespace CarRental.Data.Interfaces;
+
+public interface ICarMechanics
+{
+    internal bool TryReplaceComponent(Car car, Engine engine);
+    internal bool TryReplaceComponent(Car car, Transmission transmission);
+    internal bool TryReplaceComponent(Car car, Interior interior);
+    internal bool TryReplaceComponent(Car car, Wheels wheels);
+    internal bool TryReplaceComponent(Car car, TypeTire tire);
+    internal void Paint(Car car, KnownColor color);
+
+    public ComponentStatus CheckComponent(IComponent component);
+    public bool TryFixComponent(IComponent component);
+
+    internal float CheckFuel(Car car);
+    internal void Refill(Car car);
+}
