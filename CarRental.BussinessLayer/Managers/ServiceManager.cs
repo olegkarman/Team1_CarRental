a08253c16b248e2f3d3728d3de8997a5f5237618
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-06-06 22:49:22 +0300

    Fix ServiceManager class. Move ServiceManagerSupplements into logic level.

--- CarRental.BussinessLayer/Managers/ServiceManager.cs
+++ CarRental.BussinessLayer/Managers/ServiceManager.cs
@@ -7,6 +7,7 @@ using CarRental.Data.Enums;
 using CarRental.Data.Interfaces;
 using CarRental.Data.Models.Car;
 using CarRental.Data.Models.Car.RecordTypes;
+using CarRental.BussinessLayer.Managers;
 
 
 // HILLEL, C# PRO COURSE, TEACHER: MARIIA DZIVINSKA
--- CarRental.BussinessLayer/Managers/ServiceManagerSupplements.cs
+++ CarRental.BussinessLayer/Managers/ServiceManagerSupplements.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CarRental.Models.Car.RecordTypes;
+using CarRental.Data.Models.Car;
+using CarRental.Data.Models.Car.RecordTypes;
+using CarRental.BussinessLayer.Managers;
+using CarRental.BussinessLayer.Interfaces;
 
 // HILLEL, C# PRO COURSE, TEACHER: MARIIA DZIVINSKA
 // HOMEWORK: "ДЗ 4. Extension methods & Record type."
 // STUDENT: PARKHOMENKO YAROSLAV
 // DATE: 03-JUN-2024
 
-namespace CarRental.Data.Models.Car;
+namespace CarRental.BussinessLayer.Managers;
 
 public class ServiceManagerSupplements
 {
