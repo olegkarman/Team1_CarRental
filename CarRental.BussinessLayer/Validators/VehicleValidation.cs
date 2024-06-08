54f9fbeb6851ec2a40fe342356ee60fd81ffa4c2
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-06-08 15:44:06 +0300

    Delete search methods from validation class.

--- CarRental.BussinessLayer/Validators/VehicleValidation.cs
+++ CarRental.BussinessLayer/Validators/VehicleValidation.cs
@@ -13,48 +13,6 @@ namespace CarRental.BussinessLayer.Validators
     {
         // METHODS
 
-        public Car ChooseCarFromList(List<Car> list, int index)
-        {
-
-            CheckNull(list);
-
-            if ((index < 0) || (index > list.Count))
-            {
-                throw new IndexOutOfRangeException();
-            }
-
-            Car car = list[index];
-
-            CheckNull(car);
-
-            return car;
-        }
-
-        public Car ChooseCarFromList(List<Car> list, string model)
-        {
-            CheckNull(list);
-
-            try
-            {
-                // THE EMPTY LINE CAN APPEAR.
-                return list.Find(x => x.Model.Contains(model));
-            }
-            catch (IndexOutOfRangeException exception)
-            {
-                throw exception;
-            }
-            catch (NullReferenceException exception)
-            {
-                throw exception;
-            }
-            catch (FormatException exception)
-            {
-                throw exception;
-            }
-        }
-
-        // NULL CHECK
-
         public void CheckNull(Car car)
         {
             if (car == null)
