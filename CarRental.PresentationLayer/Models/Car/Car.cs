9358ecc3660a35957a68bb30ad60e29b8e850b01
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-06-15 22:16:27 +0300

    Add lis of Inspection into main Car-class.

--- CarRental.PresentationLayer/Models/Car/Car.cs
+++ CarRental.PresentationLayer/Models/Car/Car.cs
@@ -107,6 +107,7 @@ public class Car : ICar
     public string Model { get; init; }
     public string Brand { get; init; }
     public TransportStatus Status { get; set; }
+    public List<Inspection.Inspection> Inspections { get; set; }
     public bool IsFitForUse { get; set; }
 
     // CONSTRUCTORS
