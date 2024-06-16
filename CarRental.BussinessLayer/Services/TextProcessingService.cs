c5abc8df1e73c2ac4d2824a8bf19534f1d43beb6
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-06-16 17:26:45 +0300

    Create text-processing class. Move validator class into logic level.

---
+++ CarRental.BussinessLayer/Services/TextProcessingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CarRental.BussinessLayer.Services
+{
+    public class TextProcessingService
+    {
+        // FIELDS
+
+        // PROPERTIES
+
+        // CONSTRUCTORS
+
+        // METHODS
+
+        public string ReplaceSpacesByEmpty(string inputString)
+        {
+            return Regex.Replace(inputString, @"\s+", string.Empty);
+        }
+    }
+}
