eab0ef2174b0a40fee291188d9efee6fab9c815b
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-06-28 17:10:50 +0300

    Add Brand manager class.

---
+++ CarRental.BussinessLayer/Managers/BrandManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Data.Models.Automobile.RecordTypes;
+
+namespace CarRental.BussinessLayer.Managers
+{
+    public class BrandManager
+    {
+        // FIELDS
+
+        private const string _noInfo = "NO INFORMATION";
+
+        // PROPERTIES
+
+        public List<Brand> Brands { get; set; }
+
+        // CONSTRUCTORS
+
+        public BrandManager()
+        {
+            Brands = new List<Brand>();
+        }
+
+        // METHODS
+
+        // CREATE
+
+        public Brand GetNewBrand(Guid id, string name, string[] models)
+        {
+            Brand brand = new Brand
+            {
+                Id = id,
+                BrandName = name,
+                Models = models
+            };
+
+            return brand;
+        }
+
+        // RETRIVE
+
+        public Brand GetBrandFromList(List<Brand> brands, int index)
+        {
+            Brand brand = brands[index];
+
+            return brand;
+        }
+
+        public Brand GetBrandFromList(List<Brand> brands, Guid id)
+        {
+            Brand brand = brands.Find(x => x.Id.CompareTo(id) == 0);
+
+            return brand;
+        }
+
+        public string ShowBrandInfo(Brand brand)
+        {
+            StringBuilder modelsBuilder = new StringBuilder();
+
+            foreach(string model in brand.Models)
+            {
+                modelsBuilder.Append(model);
+                modelsBuilder.Append(" | ");
+            }
+
+            return brand.ToString() + " " + modelsBuilder.ToString();
+        }
+
+        // UPDATE
+
+        public void AddBrandInToList(List<Brand> brands, Brand brand)
+        {
+            brands.Add(brand);
+        }
+
+        // DELETE
+    }
+}
--- CarRental.PresentationLayer/Models/Automobile/RecordTypes/Brand.cs
+++ CarRental.PresentationLayer/Models/Automobile/RecordTypes/Brand.cs
@@ -4,22 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CarRental.Data.Interfaces;
-using CarRental.Data.Models.Automobile.RecordTypes;
 
-namespace CarRental.Data.Models.Automobile;
+namespace CarRental.Data.Models.Automobile.RecordTypes;
 
-public class Brand
+public record class Brand
 {
     // THE PURPOSE OF THE CLASS:
     // // A CLASS TO HOLD INFO ABOUT BRANDS OF CARS.
 
     // FIELDS
 
-    private const string _noInfo = "NO INFO";
+    private const string _noInfo = "NO INFORMATION";
 
     // PROPERTIES
 
-    public string RecordId { get; init; }
+    public Guid Id { get; init; }
     public string BrandName { get; init; }
     public string[] Models { get; init; }
     /*public Dictionary<string, ICarSelectivePattern> ModelPattern { get; set; }*/
@@ -28,14 +27,13 @@ public class Brand
 
     public Brand()
     {
-        
+
     }
 
-    public Brand(string recordId, string brandName, string[] models/*, Dictionary<string, ICarSelectivePattern> dictionaryModelPattern*/)
+    public Brand(Guid recordId, string brandName, string[] models/*, Dictionary<string, ICarSelectivePattern> dictionaryModelPattern*/)
     {
-        this.RecordId = recordId;
-        this.BrandName = brandName;
-        this.Models = models;
-        //this.ModelPattern = dictionaryModelPattern;
+        Id = recordId;
+        BrandName = brandName;
+        Models = models;
     }
 }
