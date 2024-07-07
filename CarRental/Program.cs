12b9c25d71368295a811cec7a010c7efa62d2512
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-07-07 23:18:58 +0300

    Add folder for so-called Database migrations. Add 'FluentMigrator'-program to Data-layer of application. Add ConfigurationBuilder and IConfigurationRoot (kind of data collection type) instances.

--- CarRental/Program.cs
+++ CarRental/Program.cs
@@ -2,6 +2,7 @@
 using CarRental.Data.Models.Login;
 using CarRental.Data.Models.Gateway;
 using CarRental.Presentation.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace CarRental.Presentation;
 
@@ -9,14 +10,25 @@ class CarRentalPortal
 {
     static void Main(string[] args)
     {
-        var consoleOutput = new ConsoleOutput();
-        ConsoleHelper.ConsoleHelper.ApplyConsoleStyles();
-        var login = new Login();
-        var loginManager = new LoginManager(login, consoleOutput);
-        var (user, isCustomer) = loginManager.StartLogin(); // THE CUSTOMER-MANAGER CREATES A CUSTOMER INSTANCE, NOT SOME PRESENTATION LAYER LOGIC. AT LEAST IT SHOULD... I THINK... O_o
-        var portal = new Portal(user, isCustomer);
-        var portalManager = new PortalManager(portal, consoleOutput);
-        ConsoleHelper.ConsoleHelper.ClearConsoleWithDelay(2);
-        portalManager.StartMainMenu();
+        //var consoleOutput = new ConsoleOutput();
+        //ConsoleHelper.ConsoleHelper.ApplyConsoleStyles();
+        //var login = new Login();
+        //var loginManager = new LoginManager(login, consoleOutput);
+        //var (user, isCustomer) = loginManager.StartLogin(); // THE CUSTOMER-MANAGER CREATES A CUSTOMER INSTANCE, NOT SOME PRESENTATION LAYER LOGIC. AT LEAST IT SHOULD... I THINK... O_o
+        //var portal = new Portal(user, isCustomer);
+        //var portalManager = new PortalManager(portal, consoleOutput);
+        //ConsoleHelper.ConsoleHelper.ClearConsoleWithDelay(2);
+        //portalManager.StartMainMenu();
+
+        DatabaseContextDapper dataContext = new DatabaseContextDapper();
+
+        IConfigurationRoot configurations = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.YarikSuper.json")
+            .Build();
+
+        string connectionString = configurations.GetConnectionString("YParkhomenko");
+
+        Console.WriteLine(connectionString);
+
     }
 }
