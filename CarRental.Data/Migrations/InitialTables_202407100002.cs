6f27a801a371e46b4e5f340dfc2660acae4a8d2c
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-07-10 05:56:46 +0300

    Add Cars migration. WORK IN PROGRESS.

--- CarRental.Data/Migrations/InitialTables_202407100002.cs
+++ CarRental.Data/Migrations/InitialTables_202407100002.cs
@@ -7,9 +7,9 @@ using FluentMigrator;
 
 namespace CarRentalData.Migrations
 {
-    [Migration(202407100001)]
+    [Migration(202407100002)]
 
-    public class InitialTables_202407100001 : Migration
+    public class InitialTables_202407100002 : Migration
     {
         public override void Up()
         {
@@ -20,23 +20,24 @@ namespace CarRentalData.Migrations
                     (
                         SELECT TABLE_NAME
                             FROM INFORMATION_SCHEMA.TABLES
-                            WHERE TABLE_NAME = 'Mechanicists'
+                            WHERE TABLE_NAME = 'Cars'
                     )
                         BEGIN
-                            CREATE TABLE Mechanicists
+                            CREATE TABLE Cars
                             (
-                                Id NVARCHAR(100) NOT NULL,
-                                CONSTRAINT PK_Mechanicists_Id
-                                    PRIMARY KEY (Id),
+                                CarId NVARCHAR(100) NOT NULL,
+                                VinCode NVARCHAR(100) NOT NULL,
+                                CONSTRAINT PK_Cars_CarId_VinCode
+                                    PRIMARY KEY (CarId, VinCode),
                                 Year INT NOT NULL,
                                 Name NVARCHAR(150) NOT NULL,
                                 Surename NVARCHAR(150) NOT NULL
                             );
 
-                            PRINT 'MIGRATION APPLIED SUCCESSFULLY: TABLE Mechanicists CREATED';
+                            PRINT 'MIGRATION APPLIED SUCCESSFULLY: TABLE Cars CREATED';
                         END
                     ELSE
-                        PRINT 'MIGRATION FAILED: TABLE Mechanicists IS ALREADY EXIST';
+                        PRINT 'MIGRATION FAILED: TABLE Cars IS ALREADY EXIST';
                 "
             );
         }
@@ -50,15 +51,15 @@ namespace CarRentalData.Migrations
                     (
                         SELECT TABLE_NAME
                             FROM INFORMATION_SCHEMA.TABLES
-                            WHERE TABLE_NAME = 'Mechanicists'
+                            WHERE TABLE_NAME = 'Cars'
                     )
                         BEGIN
-                            DROP TABLE Mechanicists;
+                            DROP TABLE Cars;
 
-                            PRINT 'MIGRATION APPLIED SUCCESSFULLY: TABLE Mechanicists DELETED';
+                            PRINT 'MIGRATION APPLIED SUCCESSFULLY: TABLE Cars DELETED';
                         END
                     ELSE
-                        PRINT 'MIGRATION FAILED: TABLE Mechanicists IS NOT EXIST';     
+                        PRINT 'MIGRATION FAILED: TABLE Cars IS NOT EXIST';     
                 "
             );
         }
