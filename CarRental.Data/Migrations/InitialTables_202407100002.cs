5004fc11f9bc6554c4928a446315a0ac3b928755
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-07-10 06:14:59 +0300

    Add Cars migration.

--- CarRental.Data/Migrations/InitialTables_202407100002.cs
+++ CarRental.Data/Migrations/InitialTables_202407100002.cs
@@ -29,9 +29,27 @@ namespace CarRentalData.Migrations
                                 VinCode NVARCHAR(100) NOT NULL,
                                 CONSTRAINT PK_Cars_CarId_VinCode
                                     PRIMARY KEY (CarId, VinCode),
-                                Year INT NOT NULL,
-                                Name NVARCHAR(150) NOT NULL,
-                                Surename NVARCHAR(150) NOT NULL
+                                CustomerId NVARCHAR(100) NULL,
+                                NumberPlate NVARCHAR(50) NOT NULL,
+                                Brand NVARCHAR(500) NOT NULL,
+                                Model NVARCHAR(500) NOT NULL,
+                                Price INT NOT NULL,
+                                NumberOfSeats INT NULL,
+                                NumberOfDoors INT NULL,
+                                Mileage FLOAT NULL,
+                                MaxFuelCapacity INT NULL,
+                                CurrentFuel FLOAT NULL,
+                                Year DATE NULL,
+                                IsFitForUse BIT NULL,
+                                Engine NVARCHAR(500) NULL,
+                                Transmission NVARCHAR(500) NULL,
+                                Interior NVARCHAR(500) NULL,
+                                Wheels NVARCHAR(500) NULL,
+                                Lights NVARCHAR(500) NULL,
+                                Signal NVARCHAR(500) NULL,
+                                Color NVARCHAR(500) NULL,
+                                DealId NVARCHAR(100),
+                                StatusId INT NULL
                             );
 
                             PRINT 'MIGRATION APPLIED SUCCESSFULLY: TABLE Cars CREATED';
