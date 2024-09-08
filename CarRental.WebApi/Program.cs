d320bee29f6df014ca06d4bad2354b34f03017b2
Yaroslav Parkhomenko <YaroslavParkhomenkoPetrovich@gmail.com>
2024-09-08 11:45:59 +0300

    Copy configurations to Web Api from Console presentation.

--- CarRental.WebApi/Program.cs
+++ CarRental.WebApi/Program.cs
@@ -1,5 +1,8 @@
 using CarRental.BussinessLayer.Managers;
 using CarRental.BussinessLayer.Interfaces;
+using System.Reflection;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Hosting;
 
 namespace CarRental.WebApi
 {
@@ -17,6 +20,32 @@ namespace CarRental.WebApi
             builder.Services.AddSwaggerGen();
             builder.Services.AddSingleton<ICarManager, ServiceManager>();
 
+            builder.Configuration
+                .AddJsonFile("appsettings.YarikSuper.json");
+
+            string assemblyName = builder.Configuration["NameOfDataLayerAssembly"];
+
+            Assembly.Load(assemblyName);
+
+            Assembly assembly = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(x => x.GetName().Name == assemblyName);
+
+            string connectionString = builder.Configuration.GetConnectionString("Local");
+
+            builder.Host.ConfigureServices
+            (
+                (context, services) =>
+                {
+                    services.AddLogging(c => c.AddFluentMigratorConsole())
+                    .AddFluentMigratorCore()
+                    .ConfigureRunner
+                    (
+                        c => c.AddSqlServer2016()
+                        .WithGlobalConnectionString(connectionString)
+                        .ScanIn(assembly).For.Migrations()
+                    );
+                }
+            );
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -26,12 +55,22 @@ namespace CarRental.WebApi
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
             app.MapControllers();
 
+            using (IServiceScope scope = app.Services.CreateScope())
+            {
+                var migrationManager = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+                //migrationManager.ListMigrations();
+                //migrationManager.MigrateDown(202407090001);
+                //migrationManager.MigrateUp();
+                migrationManager.ListMigrations();
+            }
+
+            // SINGLETON
             var carManager = app.Services.GetRequiredService<ICarManager>();
 
             carManager.InitializeManagment();
